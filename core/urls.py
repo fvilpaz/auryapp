c14338d4447c13d3a1eca449ac28639bf7a058eb
Fernando Vilas Paz <136896146+fvilpaz@users.noreply.github.com>
2026-04-07 07:59:58 +0200

    Actualizar urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('eventos/', views.lista_eventos, name='lista_eventos'),
+    path('eventos/json/', views.eventos_json, name='eventos_json'),
+    path('calendario/', views.calendario, name='calendario'),
 ]
